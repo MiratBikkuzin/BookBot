3bc627f84c29f207f877457fd655687ec3d201ff
Mirat <mirat1909@gmail.com>
2023-12-20 18:28:20 +0500

    feat: add async func to connect to db

---
+++ models/methods.py
@@ -0,0 +1,21 @@
+from config_data.config import Config
+from aiomysql.utils import _PoolAcquireContextManager
+from asyncio import ProactorEventLoop
+from aiomysql import Pool, create_pool
+
+
+async def db_connection(running_loop: ProactorEventLoop, config: Config) -> tuple[Pool, _PoolAcquireContextManager]:
+
+    pool: Pool = create_pool(
+        loop=running_loop,
+        host=config.db.host,
+        port=config.db.port,
+        db=config.db.name,
+        user=config.db.user,
+        password=config.db.password
+    )
+
+    async with pool.acquire() as connection:
+        pass
+
+    return pool, connection
