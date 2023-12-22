19d9d86618cd674c98911d7e9940517768841e2b
Mirat <mirat1909@gmail.com>
2023-12-22 21:58:09 +0500

    feat: add a handler for answer other user messages

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,4 +1,12 @@
+from lexicon.lexicon import LEXICON_RU
+
 from aiogram import Router
+from aiogram.types import Message
 
 
 router: Router = Router(name='OtherRouter')
+
+
+@router.message()
+async def echo(message: Message):
+    await message.answer(LEXICON_RU['other_text'])
