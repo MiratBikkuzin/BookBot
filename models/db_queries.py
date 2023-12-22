b175387f28f889c4685e198728951968de122f2d
Mirat <mirat1909@gmail.com>
2023-12-22 23:52:02 +0500

    chore: add query to select user info from db

--- models/db_queries.py
+++ models/db_queries.py
@@ -1,3 +1,9 @@
 add_user_info_query: str = """
 INSERT INTO `users_info` (`user_id`, `page`)
 VALUES (%s, %s)"""
+
+
+select_user_info_query: str = """
+SELECT `user_id`, `page`
+FROM `users_info`
+WHERE `user_id` = %s"""
