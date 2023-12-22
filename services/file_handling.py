c31770d92604e7aa74cd718750596bd2159f241c
Mirat <mirat1909@gmail.com>
2023-12-22 20:49:15 +0500

    refactor: update func _get_part_text

--- services/file_handling.py
+++ services/file_handling.py
@@ -8,7 +8,7 @@ PAGE_SIZE: int = 1100
 def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
     edit_text: str = re.sub(r'[.,!?:;]\.*$', '...', text[start:start + size])
     edit_text: str = re.findall(r'(?s).+[.,!?:;]', edit_text)
-    edit_text: str = re.sub(r'\n(?!    )', ' ', edit_text[0])  # removes unnecessary \n 
+    edit_text: str = edit_text[0].strip()
     return edit_text, len(edit_text)
 
 
