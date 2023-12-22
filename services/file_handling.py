73e4ea939e316e3a4ddb94e026b8fe9f3ff17e2b
Mirat <mirat1909@gmail.com>
2023-12-22 20:57:22 +0500

    feat: add a func to normalize book page

--- services/file_handling.py
+++ services/file_handling.py
@@ -5,6 +5,9 @@ BOOK_PATH: str = 'book/book.txt'
 PAGE_SIZE: int = 1100
 
 
+book: dict[int: str] = {}
+
+
 def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
     edit_text: str = re.sub(r'[.,!?:;]\.*$', '...', text[start:start + size])
     edit_text: str = re.findall(r'(?s).+[.,!?:;]', edit_text)
@@ -13,4 +16,17 @@ def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
 
 
 def prepare_book(path: str) -> None:
-    pass
+    
+    with open(path, 'rt', encoding='utf-8') as book_file:
+        book_text: str = book_file.read().replace('\n\n', '\n')
+
+    start, page_num = 0, 1
+
+    while start < len(book_text):
+        part_text, part_size = _get_part_text(book_text, start, PAGE_SIZE)
+        book[page_num] = part_text
+        start += part_size
+        page_num += 1
+
+
+prepare_book(os.path.join(sys.path[0], os.path.normpath(BOOK_PATH)))
