e2c85ab9244ff916a2c0b3efa45f1bb938d410da
Mirat <mirat1909@gmail.com>
2023-12-23 16:43:29 +0500

    chore: add query to update user book page

--- models/db_queries.py
+++ models/db_queries.py
@@ -3,6 +3,11 @@ INSERT INTO `users_info` (`user_id`, `page`)
 VALUES (%s, %s)"""
 
 
+update_user_page_query: str = """
+UPDATE `users_info`
+SET `page` = %s"""
+
+
 select_user_info_query: str = """
 SELECT `user_id`, `page`
 FROM `users_info`
