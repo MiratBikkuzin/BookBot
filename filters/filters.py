c1f2fa74f039133d9ecef018128d733cc59dba6b
Mirat <mirat1909@gmail.com>
2023-12-23 21:35:50 +0500

    feat: add a filter to check add bookmark callback data

--- filters/filters.py
+++ filters/filters.py
@@ -2,6 +2,12 @@ from aiogram.filters import BaseFilter
 from aiogram.types import CallbackQuery
 
 
+class IsAddBookmarkCallbackData(BaseFilter):
+    async def __call__(self, callback: CallbackQuery) -> bool:
+        callback_data: str = callback.data
+        return '/' in callback_data and callback_data.replace('/', '', 1).isdigit()
+
+
 class IsDigitCallbackData(BaseFilter):
     async def __call__(self, callback: CallbackQuery) -> bool:
         return callback.data.isdigit()
