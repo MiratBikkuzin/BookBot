fd1a665a5e1fc53062f8f2c5f809cbb84c2451b0
Mirat <mirat1909@gmail.com>
2023-12-24 10:21:22 +0500

    feat: add a handler to edit bookmarks

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -116,3 +116,20 @@ async def process_add_bookmark(callback: CallbackQuery, bookmark_page: int) -> N
         )
 
 
+@router.callback_query(F.data == 'edit_bookmarks')
+async def process_edit_bookmark(callback: CallbackQuery) -> None:
+    
+    user_bookmarks: iter[int] = chain.from_iterable(
+        await execute_query(
+            select_user_bookmarks_query,
+            'SELECT_ALL',
+            callback.from_user.id
+        )
+    )
+
+    await callback.message.edit_text(
+        text=LEXICON_RU['edit_bookmarks'],
+        reply_markup=create_edit_kb(*user_bookmarks)
+    )
+
+
