fedf366e4375594a3db4521c2fed8e4516e33348
Mirat <mirat1909@gmail.com>
2023-12-24 11:34:15 +0500

    refactor: move handlers

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -118,21 +118,6 @@ async def process_add_bookmark(callback: CallbackQuery, bookmark_page: int) -> N
         )
 
 
-@router.callback_query(F.data == 'edit_bookmarks')
-async def process_edit_bookmark(callback: CallbackQuery) -> None:
-    
-    user_bookmarks: iter[int] = chain.from_iterable(
-        await execute_query(select_user_bookmarks_query,
-                            'SELECT_ALL',
-                            callback.from_user.id)
-    )
-
-    await callback.message.edit_text(
-        text=LEXICON_RU['edit_bookmarks'],
-        reply_markup=create_edit_kb(*user_bookmarks)
-    )
-
-
 @router.callback_query(F.data == 'forward')
 async def process_page_forward(callback: CallbackQuery) -> None:
 
@@ -175,3 +160,16 @@ async def process_bookmark_press(callback: CallbackQuery) -> None:
     await callback.message.edit_text(book[int(callback.data)])
 
 
+@router.callback_query(F.data == 'edit_bookmarks')
+async def process_edit_bookmark(callback: CallbackQuery) -> None:
+    
+    user_bookmarks: iter[int] = chain.from_iterable(
+        await execute_query(select_user_bookmarks_query,
+                            'SELECT_ALL',
+                            callback.from_user.id)
+    )
+
+    await callback.message.edit_text(
+        text=LEXICON_RU['edit_bookmarks'],
+        reply_markup=create_edit_kb(*user_bookmarks)
+    )
