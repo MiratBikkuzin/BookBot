79983190133cd228fe7e1bcd494e2607576972ad
Mirat <mirat1909@gmail.com>
2023-12-24 11:09:46 +0500

    feat: add a handler to move book page backward

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -151,3 +151,22 @@ async def process_page_forward(callback: CallbackQuery) -> None:
     )
 
 
+
+@router.callback_query(F.data == 'backward')
+async def process_page_backward(callback: CallbackQuery) -> None:
+
+    user_id, user_book_page = await execute_query(select_user_info_query, 'SELECT_ONE', callback.from_user.id)
+    user_book_page -= 1
+
+    await execute_query(update_user_page_query, 'UPDATE',
+                        user_book_page, user_id)
+    await callback.message.edit_text(
+        text=book[user_book_page],
+        reply_markup=create_pagination_kb(
+            'backward',
+            f'{user_book_page}/{len(book)}',
+            'forward'
+        )
+    )
+
+
