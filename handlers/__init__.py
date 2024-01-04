4c747f18a72dbda5d3a0011f0a125bfd696b0bcf
Mirat <mirat1909@gmail.com>
2024-01-04 23:46:55 +0500

    refactor: rename a folder user_handlers to user

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,2 +1,2 @@
-from handlers.user_handlers import (start_handlers, book_handlers,
+from handlers.user import (start_handlers, book_handlers,
                                     bookmark_handlers, other_handlers)
