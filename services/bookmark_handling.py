c177c364563b997a8e35225ba0f81af14b72e801
Mirat <mirat1909@gmail.com>
2024-01-04 10:26:38 +0500

    fix: fix uncorrect name of the variables

--- services/bookmark_handling.py
+++ services/bookmark_handling.py
@@ -1,4 +1,4 @@
-from db.db_queries import select_user_bookmarks_query
+from db.db_queries import user_bookmarks_query
 from db.methods import execute_query
 
 from itertools import chain
@@ -6,7 +6,7 @@ from itertools import chain
 
 async def get_user_bookmarks(user_id: int) -> tuple[int]:
     return tuple(chain.from_iterable(await execute_query(
-        select_user_bookmarks_query,
+        user_bookmarks_query,
         'SELECT_ALL',
         user_id)
     ))
