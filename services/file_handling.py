21cb7bd4a7488e2df270bc96556105d32a692d4c
Mirat <mirat1909@gmail.com>
2024-01-04 23:41:13 +0500

    refactor: update a func prepare_book

--- services/file_handling.py
+++ services/file_handling.py
@@ -1,11 +1,11 @@
-import os, sys, re
+import io
+import re
 
 
-BOOK_PATH: str = 'book/book.txt'
 PAGE_SIZE: int = 1100
 
 
-book: dict[int: str] = {}
+book: dict[str: dict[int: str]] = {}
 
 
 def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
@@ -15,18 +15,15 @@ def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
     return edit_text, len(edit_text)
 
 
-def prepare_book(path: str) -> None:
-    
-    with open(path, 'rt', encoding='utf-8') as book_file:
-        book_text: str = book_file.read().replace('\n\n', '\n')
+def prepare_book(path: io.BytesIO, book_name: str) -> None:
+
+    book_text: str = path.read().decode('utf-8').replace('\n\n', '\n')
+    book[book_name] = {}
 
     start, page_num = 0, 1
 
     while start < len(book_text):
         part_text, part_size = _get_part_text(book_text, start, PAGE_SIZE)
-        book[page_num] = part_text
+        book[book_name][page_num] = part_text
         start += part_size + 1
         page_num += 1
-
-
-prepare_book(os.path.join(sys.path[0], os.path.normpath(BOOK_PATH)))
