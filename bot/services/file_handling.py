c4934875cc067179f682fcc173f8efddf418f9b2
Mirat <mirat1909@gmail.com>
2024-01-05 23:08:13 +0500

    refactor: update a func prepare book

--- bot/services/file_handling.py
+++ bot/services/file_handling.py
@@ -1,5 +1,5 @@
-import io
 import re
+from typing import BinaryIO
 
 
 PAGE_SIZE: int = 1100
@@ -15,9 +15,10 @@ def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
     return edit_text, len(edit_text)
 
 
-def prepare_book(byte_book_text: io.BytesIO, book_name: str) -> None:
+def prepare_book(binary_book: BinaryIO, book_name: str) -> None:
 
-    book_text: str = byte_book_text.read().decode('utf-8').replace('\n\n', '\n')
+    with binary_book:
+        book_text: str = binary_book.read().decode('utf-8').replace('\n\n', '\n')
 
     start, page_num = 0, 1
 
