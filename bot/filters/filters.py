7f43f7f16c0147b8e7cf87fa7b18cafc81bcc005
Mirat <mirat1909@gmail.com>
2024-01-05 21:53:47 +0500

    chore: add a filter for check correct document format

--- bot/filters/filters.py
+++ bot/filters/filters.py
@@ -7,6 +7,12 @@ from aiogram.types import Message, CallbackQuery
 class IsAdmin(BaseFilter):
     async def __call__(self, message: Message) -> bool:
         return message.from_user.id in load_config().tg_bot.admin_ids
+    
+
+class IsCorrectDocumentFormat(BaseFilter):
+    async def __call__(self, message: Message) -> bool | None:
+        if message.document:
+            return message.document.file_name.split('.')[-1] in ('fb2', 'txt')
 
 
 class IsAddBookmarkCallbackData(BaseFilter):
