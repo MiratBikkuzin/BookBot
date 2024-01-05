9cc4192374af023edc632775576e1a73e26c3c21
Mirat <mirat1909@gmail.com>
2024-01-05 14:55:00 +0500

    feat: add a filter for check admin

--- bot/filters/filters.py
+++ bot/filters/filters.py
@@ -1,9 +1,16 @@
+from config_data.config import load_config
+
 from aiogram.filters import BaseFilter
-from aiogram.types import CallbackQuery
+from aiogram.types import Message, CallbackQuery
+
+
+class IsAdmin(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id in load_config().tg_bot.admin_ids
 
 
 class IsAddBookmarkCallbackData(BaseFilter):
-    async def __call__(self, callback: CallbackQuery) -> bool:
+    async def __call__(self, callback: CallbackQuery) -> dict[str: int] | None:
         callback_data: str = callback.data
         if '/' in callback_data and callback_data.replace('/', '', 1).isdigit():
             return {'bookmark_page': int(callback_data.split('/')[0])}
@@ -15,7 +22,7 @@ class IsDigitCallbackData(BaseFilter):
     
 
 class IsDelBookmarkCallbackData(BaseFilter):
-    async def __call__(self, callback: CallbackQuery) -> bool:
+    async def __call__(self, callback: CallbackQuery) -> dict[str: int] | None:
         callback_data: str = callback.data
         if callback_data.endswith('del') and callback_data[:-3].isdigit():
             return {'del_bookmark_page': int(callback_data[:-3])}
