d48d0ed8e033e131e1ccffcd5a2a6779334d86d9
Mirat <mirat1909@gmail.com>
2024-01-05 22:30:46 +0500

    feat: add the handlers for send admin book title

--- bot/handlers/admin/admin_book_handlers.py
+++ bot/handlers/admin/admin_book_handlers.py
@@ -1,11 +1,12 @@
 from db.db_queries import add_admin_books, admin_books_query, admin_books_count_query
 from db.methods import execute_query
-from filters.filters import IsAdmin
+from filters.filters import IsAdmin, IsCorrectDocumentFormat
 from states.states import default_state, FSMAdminBook
+from services.file_handling import book, prepare_book
 from lexicon.lexicon import LEXICON_RU
 
-from aiogram import Router
-from aiogram.types import Message, InputMediaDocument
+from aiogram import Router, F
+from aiogram.types import Message
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 
@@ -16,9 +17,21 @@ router: Router = Router(name=__name__)
 @router.message(Command(commands='update'), IsAdmin(), StateFilter(default_state))
 async def process_update_command(message: Message, state: FSMContext):
     await message.answer(text=LEXICON_RU[message.text])
+    await state.set_state(FSMAdminBook.admin_book_title)
+
+
+@router.message(Command(commands='update'), ~IsAdmin(), StateFilter(default_state))
+async def not_update_command_warning(message: Message):
+    await message.answer(text=LEXICON_RU['other_update_command'])
+
+
+@router.message(IsAdmin(), StateFilter(FSMAdminBook.admin_book_title), F.text)
+async def admin_book_title_send(message: Message, state: FSMContext):
+    book[message.text] = {}
+    await message.answer(text=LEXICON_RU['admin_sent_book_title'])
     await state.set_state(FSMAdminBook.admin_book_document)
 
 
-@router.message(Command(commands='update'), StateFilter(default_state))
-async def not_update_warning(message: Message):
-    await message.answer(text=LEXICON_RU['other_update_command'])
+@router.message(StateFilter(FSMAdminBook.admin_book_title), ~F.text)
+async def not_admin_book_title_warning(message: Message):
+    await message.answer(text=LEXICON_RU['other_admin_sent_book_title'])
--- bot/states/states.py
+++ bot/states/states.py
@@ -2,10 +2,10 @@ from aiogram.fsm.state import default_state, State, StatesGroup
 
 
 class FSMAdminBook(StatesGroup):
-    admin_book_name = State()
+    admin_book_title = State()
     admin_book_document = State()
 
 
 class FSMUserBook(StatesGroup):
-    user_book_name = State()
+    user_book_title = State()
     user_book_document = State()
