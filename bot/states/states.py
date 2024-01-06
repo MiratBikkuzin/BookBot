f61afa3cdf1512955cf749846b11c1050a19eded
Mirat <mirat1909@gmail.com>
2024-01-06 17:28:47 +0500

    refactor: update states

--- bot/states/states.py
+++ bot/states/states.py
@@ -2,10 +2,8 @@ from aiogram.fsm.state import default_state, State, StatesGroup
 
 
 class FSMAdminBook(StatesGroup):
-    admin_book_title = State()
-    admin_book_document = State()
+    admin_book_send = State()
 
 
 class FSMUserBook(StatesGroup):
-    user_book_title = State()
-    user_book_document = State()
+    user_book_send = State()
