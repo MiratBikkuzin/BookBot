dd2aeb26b741efc228add87ea1f477c4af29301b
Mirat Bikkuzin <mirat1909@gmail.com>
2024-01-11 13:40:24 +0500

    update for linux bash

--- bot/handlers/admin/admin_book_handlers.py
+++ bot/handlers/admin/admin_book_handlers.py
@@ -1,11 +1,11 @@
-from db.db_queries import add_admin_books, admin_books_query, admin_books_count_query
+from db.db_queries import add_admin_books
 from db.methods import execute_query
-from filters.filters import IsAdmin, IsCorrectDocumentFormat
-from states.states import default_state, FSMAdminBook
-from services.file_handling import book, prepare_book
+from filters.filters import IsAdmin, IsCorrectAdminBook
+from states.states import FSMAdminBook, default_state
+from services.file_handling import prepare_book
 from lexicon.lexicon import LEXICON_RU
 
-from aiogram import Router, F
+from aiogram import Router, Bot
 from aiogram.types import Message
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
@@ -17,21 +17,28 @@ router: Router = Router(name=__name__)
 @router.message(Command(commands='update'), IsAdmin(), StateFilter(default_state))
 async def process_update_command(message: Message, state: FSMContext):
     await message.answer(text=LEXICON_RU[message.text])
-    await state.set_state(FSMAdminBook.admin_book_title)
+    await state.set_state(FSMAdminBook.admin_book_send)
 
 
-@router.message(Command(commands='update'), ~IsAdmin(), StateFilter(default_state))
-async def not_update_command_warning(message: Message):
+@router.message(Command(commands='update'), ~IsAdmin())
+async def not_update_warning(message: Message):
     await message.answer(text=LEXICON_RU['other_update_command'])
 
 
-@router.message(IsAdmin(), StateFilter(FSMAdminBook.admin_book_title), F.text)
-async def admin_book_title_send(message: Message, state: FSMContext):
-    book[message.text] = {}
-    await message.answer(text=LEXICON_RU['admin_sent_book_title'])
-    await state.set_state(FSMAdminBook.admin_book_document)
+@router.message(StateFilter(FSMAdminBook.admin_book_send), IsCorrectAdminBook())
+async def admin_send_book(message: Message, bot: Bot, book_file_id: str,
+                          book_title: str, state: FSMContext):
+    
+    if prepare_book(await bot.download(book_file_id), book_title):
+        await message.answer(text=LEXICON_RU['wait_admin_book_download'])
+        await execute_query(add_admin_books, 'INSERT', book_file_id, book_title)
+        await message.answer(text=LEXICON_RU['admin_book_download_end'])
+        await state.clear()
+
+    else:
+        await message.answer(text=LEXICON_RU['admins_book_in_stock_warning'])
 
 
-@router.message(StateFilter(FSMAdminBook.admin_book_title), ~F.text)
-async def not_admin_book_title_warning(message: Message):
-    await message.answer(text=LEXICON_RU['other_admin_sent_book_title'])
+@router.message(StateFilter(FSMAdminBook.admin_book_send), ~IsCorrectAdminBook())
+async def not_admin_send_book_warning(message: Message):
+    await message.answer(text=LEXICON_RU['other_format_admin_send_book'])
