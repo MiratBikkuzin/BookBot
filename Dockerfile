5e6f626ab9eaf32c43ec78ec4c69657943718095
Mirat Bikkuzin <mirat1909@gmail.com>
2024-01-30 22:26:57 +0500

    feat: add amazon s3 api

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ WORKDIR /bot
 
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
+RUN sudo apt install awscli -y
+RUN aws configure
 COPY . .
 
 
--- bot/config_data/config.py
+++ bot/config_data/config.py
@@ -1,15 +1,20 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from aiobotocore.session import get_session
 
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
     ADMIN_IDS: str
+
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
     DB_SCHEMA: str
+
+    ENDPOINT_S3: str
+    BUCKET_NAME: str
 
     @property
     def bot_token(self) -> str:
@@ -26,6 +31,14 @@ class Settings(BaseSettings):
     @property
     def database_schema(self) -> str:
         return self.DB_SCHEMA
+    
+    @property
+    async def s3_client(self):
+        return get_session().create_client('s3', endpoint_url=self.ENDPOINT_S3)
+    
+    @property
+    def s3_bucket(self) -> str:
+        return self.BUCKET_NAME
     
     model_config = SettingsConfigDict(env_file='.env')
 
