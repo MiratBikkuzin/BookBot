d034c7ac7239c273d48189cfe7953008269b0298
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-02 21:06:56 +0500

    refactor: update bookmarks keyboard methods

--- bot/keyboards/bookmarks_kb.py
+++ bot/keyboards/bookmarks_kb.py
@@ -1,4 +1,6 @@
-from services.file_handling import book
+from database.methods.get import get_user_bookmarks
+from services.s3_file_handling import get_book_s3
+from keyboards.keyboard_utils import BookmarksCallbackFactory, DelBookmarksCallbackFacotry
 from lexicon.lexicon import LEXICON_RU
 
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -8,14 +10,16 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 class BookmarkFactory:
 
     @staticmethod
-    def create_bookmarks_kb(bookmarks_pages: tuple[int]) -> InlineKeyboardMarkup:
+    async def create_bookmarks_kb(user_id: int, book_title: str) -> InlineKeyboardMarkup:
 
         kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+        book: dict[int: str] = await get_book_s3(book_title, user_id)
 
-        for button in sorted(bookmarks_pages):
+        for button in sorted(await get_user_bookmarks(user_id, book_title)):
             kb_builder.row(InlineKeyboardButton(
                 text=f'{button} - {book[button][:85]}',
-                callback_data=str(button)
+                callback_data=BookmarksCallbackFactory(page_number=button,
+                                                       book_title=book_title).pack()
             ))
 
         kb_builder.row(
@@ -32,14 +36,16 @@ class BookmarkFactory:
         return kb_builder.as_markup()
     
     @staticmethod
-    def create_edit_kb(bookmarks_pages: tuple[int]) -> InlineKeyboardMarkup:
+    async def create_edit_kb(user_id: int, book_title: str) -> InlineKeyboardMarkup:
 
         kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+        book: dict[int: str] = await get_book_s3(book_title, user_id)
 
-        for button in sorted(bookmarks_pages):
+        for button in sorted(await get_user_bookmarks(user_id, book_title)):
             kb_builder.row(InlineKeyboardButton(
                 text=f"{LEXICON_RU['del']} {button} - {book[button][:85]}",
-                callback_data=f'{button}del'
+                callback_data=DelBookmarksCallbackFacotry(page_number=button,
+                                                          book_title=book_title).pack()
             ))
 
         kb_builder.row(InlineKeyboardButton(
