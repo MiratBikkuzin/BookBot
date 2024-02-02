24eb2709b04325bc11ae7200cea5316f0d3339c4
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-02 20:37:26 +0500

    refactor: update database get methods

--- bot/database/methods/get.py
+++ bot/database/methods/get.py
@@ -1,43 +1,38 @@
-from database.models import UsersTable, BookmarksTable, AdminBooksTable
+from database.models import UserBooksTable, BookmarksTable, AdminBooksTable
 from database.main import Database
 
-from sqlalchemy import select, func
+from sqlalchemy import select, and_
 
 
-async def get_user_info(user_id: int) -> tuple[int, int]:
+async def get_user_info(user_id: int, book_title: str) -> tuple[int, int]:
     async with Database().session as session:
         stmt = (
-            select(UsersTable.user_id, UsersTable.page)
-            .select_from(UsersTable)
-            .where(UsersTable.user_id == user_id)
+            select(UserBooksTable.user_id, UserBooksTable.current_page_num)
+            .select_from(UserBooksTable)
+            .where(and_(UserBooksTable.user_id == user_id,
+                        UserBooksTable.book_title == book_title))
         )
         result = await session.execute(stmt)
         return result.fetchone()
     
 
-async def get_user_bookmarks(user_id: int) -> list[tuple[int]]:
+async def get_user_bookmarks(user_id: int, book_title: str) -> list[tuple[int]]:
     async with Database().session as session:
         stmt = (
-            select(BookmarksTable.bookmark_page)
+            select(BookmarksTable.page_number)
             .select_from(BookmarksTable)
-            .where(BookmarksTable.user_id == user_id)
+            .where(and_(BookmarksTable.user_id == user_id,
+                        BookmarksTable.book_title == book_title))
         )
         result = await session.execute(stmt)
         return result.fetchall()
     
 
-async def get_admin_books(admin_book_id: int) -> tuple[str, str]:
+async def get_admin_books() -> tuple[str, str]:
     async with Database().session as session:
         stmt = (
-            select(AdminBooksTable.file_tg_id, AdminBooksTable.book_title)
+            select(AdminBooksTable.book_title)
             .select_from(AdminBooksTable)
-            .where(AdminBooksTable.id == admin_book_id)
         )
         result = await session.execute(stmt)
         return result.fetchall()
-    
-
-async def get_count_admin_books() -> int:
-    async with Database().session as session:
-        result = await session.execute(select(func.count()).select_from(AdminBooksTable))
-        return int(result.fetchone()[0])
