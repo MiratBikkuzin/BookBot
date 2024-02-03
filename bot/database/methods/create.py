c91245fd5e056bc616147157f3756a82fbf64541
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-03 22:24:02 +0500

    refactor: update parameters in models into create database functions

--- bot/database/methods/create.py
+++ bot/database/methods/create.py
@@ -1,5 +1,6 @@
 from database.models import UsersTable, BookmarksTable, AdminBooksTable, UserBooksTable
 from database.main import Database
+from utils.book_utils import get_book_id
 
 
 async def add_user(user_id: int) -> None:
@@ -10,14 +11,15 @@ async def add_user(user_id: int) -> None:
 
 async def add_user_bookmark(user_id: int, book_title: str, page_number: int) -> None:
     async with Database().session as session:
-        session.add(BookmarksTable(user_id=user_id, book_title=book_title, page_number=page_number))
+        session.add(BookmarksTable(user_id=user_id, book_title=book_title,
+                                   book_id=get_book_id(book_title), page_number=page_number))
         await session.commit()
 
 
 async def add_admin_book(admin_username: str, book_title: str, page_count: int) -> None:
     async with Database().session as session:
         session.add(AdminBooksTable(admin_username=admin_username, book_title=book_title,
-                                    total_page_count=page_count)) 
+                                    book_id=get_book_id(book_title), total_page_count=page_count)) 
         await session.commit()
 
 
@@ -25,6 +27,6 @@ async def add_user_book(user_id: int, book_title: str, page_count: int,
                         is_admin_book: bool = False) -> None:
     async with Database().session as session:
         session.add(UserBooksTable(user_id=user_id, book_title=book_title,
-                                   total_page_count=page_count, current_page_num=0,
-                                   is_admin_book=is_admin_book))
+                                   book_id=get_book_id(book_title), total_page_count=page_count,
+                                   current_page_num=0, is_admin_book=is_admin_book))
         await session.commit()
