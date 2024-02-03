bb903e2b7d11f5975371d64cf155840d4c31c7c1
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-03 22:35:35 +0500

    refactor: update update database functions and add it choice for book id

--- bot/database/methods/update.py
+++ bot/database/methods/update.py
@@ -4,13 +4,13 @@ from database.main import Database
 from sqlalchemy import update, and_
 
 
-async def update_book_page(user_id: int, book_title: str, new_page: int) -> None:
+async def update_book_page(user_id: int, book_id: str, new_page: int) -> None:
     async with Database().session as session:
         stmt = (
             update(UserBooksTable)
             .values(current_page_num=new_page)
             .where(and_(UserBooksTable.user_id == user_id,
-                        UserBooksTable.book_title == book_title))
+                        UserBooksTable.book_id == book_id))
         )
         await session.execute(stmt)
         await session.commit()
