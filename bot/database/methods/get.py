7fed7148a425ae73f35e8fb099e198a9393c4573
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-03 20:34:37 +0500

    feat: add new get database method for get admin book info

--- bot/database/methods/get.py
+++ bot/database/methods/get.py
@@ -14,47 +14,58 @@ async def get_user(user_id: int) -> tuple[int]:
         )
         result = await session.execute(stmt)
         return result.fetchone()
+    
 
-
-async def get_user_book_info(user_id: int, book_title: str) -> tuple[int, int]:
+async def get_admin_book_info(book_id: int) -> tuple[str, int]:
     async with Database().session as session:
         stmt = (
-            select(UserBooksTable.user_id, UserBooksTable.total_page_count,
-                   UserBooksTable.current_page_num)
+            select(AdminBooksTable.book_title, AdminBooksTable.total_page_count)
+            .select_from(AdminBooksTable)
+            .where(AdminBooksTable.id == book_id)
+        )
+        result = await session.execute(stmt)
+        return result.fetchone()
+
+
+async def get_user_book_info(user_id: int, book_id: int) -> tuple[str, int, int]:
+    async with Database().session as session:
+        stmt = (
+            select(UserBooksTable.book_title,
+                   UserBooksTable.total_page_count, UserBooksTable.current_page_num)
             .select_from(UserBooksTable)
             .where(and_(UserBooksTable.user_id == user_id,
-                        UserBooksTable.book_title == book_title))
+                        UserBooksTable.id == book_id))
         )
         result = await session.execute(stmt)
         return result.fetchone()
     
 
-async def get_user_bookmarks(user_id: int, book_title: str) -> tuple[int]:
+async def get_user_bookmarks(user_id: int, book_id: int) -> tuple[int]:
     async with Database().session as session:
         stmt = (
             select(BookmarksTable.page_number)
             .select_from(BookmarksTable)
             .where(and_(BookmarksTable.user_id == user_id,
-                        BookmarksTable.book_title == book_title))
+                        BookmarksTable.id == book_id))
         )
         result = await session.execute(stmt)
         return tuple(chain.from_iterable(result.fetchall()))
     
 
-async def get_admin_books() -> list[tuple[str, int]]:
+async def get_admin_books() -> list[tuple[int, str, int]]:
     async with Database().session as session:
         stmt = (
-            select(AdminBooksTable.book_title, AdminBooksTable.total_page_count)
+            select(AdminBooksTable.id, AdminBooksTable.book_title, AdminBooksTable.total_page_count)
             .select_from(AdminBooksTable)
         )
         result = await session.execute(stmt)
         return result.fetchall()
     
 
-async def get_user_books(user_id: int) -> list[tuple[str, int]]:
+async def get_user_books(user_id: int) -> list[tuple[int, str, int]]:
     async with Database().session as session:
         stmt = (
-            select(UserBooksTable.book_title, UserBooksTable.total_page_count)
+            select(UserBooksTable.id, UserBooksTable.book_title, UserBooksTable.total_page_count)
             .select_from(UserBooksTable)
             .where(UserBooksTable.user_id == user_id)
         )
