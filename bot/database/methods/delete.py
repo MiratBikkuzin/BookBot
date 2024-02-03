87bfac70f6cb8c822779739d9da942a4eeab41a3
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-03 22:32:32 +0500

    refactor: update delete database functions and add it choice for book id

--- bot/database/methods/delete.py
+++ bot/database/methods/delete.py
@@ -5,42 +5,43 @@ from services.s3_file_handling import delete_book_s3
 from sqlalchemy import delete, and_
 
 
-async def del_user_bookmark(user_id: int, book_title: str, page_number: int) -> None:
+async def del_user_bookmark(user_id: int, book_id: str, page_number: int) -> None:
     async with Database().session as session:
         stmt = (
             delete(BookmarksTable)
-            .where(and_(BookmarksTable.user_id == user_id, BookmarksTable.book_title == book_title,
+            .where(and_(BookmarksTable.user_id == user_id, BookmarksTable.book_id == book_id,
                         BookmarksTable.page_number == page_number))
         )
         await session.execute(stmt)
         await session.commit()
 
 
-async def del_admin_book(admin_username: str, admin_user_id: int, book_title: str) -> None:
+async def del_admin_book(admin_username: str, admin_tg_id: int,
+                         book_title: str, book_id: str) -> None:
     
     async with Database().session as session:
         first_stmt = (
             delete(AdminBooksTable)
             .where(and_(AdminBooksTable.admin_username == admin_username,
-                        AdminBooksTable.book_title == book_title))
+                        AdminBooksTable.book_id == book_id))
         )
         second_stmt = (
             delete(UserBooksTable)
-            .where(and_(UserBooksTable.is_admin_book, UserBooksTable.book_title == book_title))
+            .where(and_(UserBooksTable.is_admin_book, UserBooksTable.book_id == book_id))
         )
         await session.execute(first_stmt)
         await session.execute(second_stmt)
         await session.commit()
 
-    await delete_book_s3(book_title, admin_user_id, True)
+    await delete_book_s3(book_title, admin_tg_id, is_admin=True)
 
 
-async def del_user_book(user_id: int, book_title: str) -> None:
+async def del_user_book(user_id: int, book_title: str, book_id: str) -> None:
     
     async with Database().session as session:
         stmt = (
             delete(UserBooksTable)
-            .where(and_(UserBooksTable.user_id == user_id, UserBooksTable.book_title == book_title))
+            .where(and_(UserBooksTable.user_id == user_id, UserBooksTable.book_id == book_id))
         )
         await session.execute(stmt)
         await session.commit()
