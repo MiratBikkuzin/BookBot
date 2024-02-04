80d774d0e90c927e7b958fc264c75134d24ac311
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-04 11:27:32 +0500

    fix: uncorrect types in callback factories book id

--- bot/keyboards/kb_utils.py
+++ bot/keyboards/kb_utils.py
@@ -3,19 +3,19 @@ from aiogram.filters.callback_data import CallbackData
 
 class AdminBookCallbackFactory(CallbackData, prefix='admin-book', sep='@'):
     total_page_count: int
-    book_id: int
+    book_id: str
 
 
 class UserBookCallbackFactory(CallbackData, prefix='user-book', sep='@'):
     total_page_count: int
-    book_id: int
+    book_id: str
 
 
 class BookmarksCallbackFactory(CallbackData, prefix='bookmark', sep='@'):
     page_number: int
-    book_id: int
+    book_id: str
 
 
 class DelBookmarksCallbackFacotry(CallbackData, prefix='del-bkmark', sep='@'):
     page_number: int
-    book_id: int
+    book_id: str
