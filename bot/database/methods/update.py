269f9205e708603016da46e35b21f204561ff4fb
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-11 11:30:58 +0500

    refactor: update update  database methods by adding database var for session instead Database class

--- bot/database/methods/update.py
+++ bot/database/methods/update.py
@@ -1,11 +1,11 @@
 from database.models import UserBooksTable
-from database.main import Database
+from database.main import database
 
 from sqlalchemy import update, and_
 
 
 async def update_book_page(user_id: int, book_id: str, new_page: int) -> None:
-    async with Database().session as session:
+    async with database.session as session:
         stmt = (
             update(UserBooksTable)
             .values(current_page_num=new_page)
