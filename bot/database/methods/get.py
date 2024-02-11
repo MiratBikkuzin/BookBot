5a866b4601eb36ea71823f19fe894d0bc73d5de3
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-11 11:30:16 +0500

    refactor: update get database methods by adding database var for session instead Database class

--- bot/database/methods/get.py
+++ bot/database/methods/get.py
@@ -1,12 +1,12 @@
 from database.models import UsersTable, UserBooksTable, BookmarksTable, AdminBooksTable
-from database.main import Database
+from database.main import database
 
 from sqlalchemy import select, and_
 from itertools import chain
 
 
 async def get_user(user_id: int) -> tuple[int]:
-    async with Database().session as session:
+    async with database.session as session:
         stmt = (
             select(UsersTable.user_id)
             .select_from(UsersTable)
@@ -17,7 +17,7 @@ async def get_user(user_id: int) -> tuple[int]:
     
 
 async def get_admin_book_info(book_id: int) -> tuple[str, int]:
-    async with Database().session as session:
+    async with database.session as session:
         stmt = (
             select(AdminBooksTable.book_title, AdminBooksTable.total_page_count)
             .select_from(AdminBooksTable)
@@ -27,11 +27,11 @@ async def get_admin_book_info(book_id: int) -> tuple[str, int]:
         return result.fetchone()
 
 
-async def get_user_book_info(user_id: int, book_id: int) -> tuple[str, int, int, bool] | None:
-    async with Database().session as session:
+async def get_user_book_info(user_id: int, book_id: int) -> tuple[int, int, bool] | None:
+    async with database.session as session:
         stmt = (
-            select(UserBooksTable.book_title, UserBooksTable.total_page_count,
-                   UserBooksTable.current_page_num, UserBooksTable.is_admin_book)
+            select(UserBooksTable.total_page_count, UserBooksTable.current_page_num,
+                   UserBooksTable.is_admin_book)
             .select_from(UserBooksTable)
             .where(and_(UserBooksTable.user_id == user_id,
                         UserBooksTable.book_id == book_id))
@@ -41,7 +41,7 @@ async def get_user_book_info(user_id: int, book_id: int) -> tuple[str, int, int,
     
 
 async def get_user_bookmarks(user_id: int, book_id: int) -> tuple[int]:
-    async with Database().session as session:
+    async with database.session as session:
         stmt = (
             select(BookmarksTable.page_number)
             .select_from(BookmarksTable)
@@ -53,7 +53,7 @@ async def get_user_bookmarks(user_id: int, book_id: int) -> tuple[int]:
     
 
 async def get_admin_books() -> list[tuple[str, str, int]]:
-    async with Database().session as session:
+    async with database.session as session:
         stmt = (
             select(AdminBooksTable.book_id, AdminBooksTable.book_title,
                    AdminBooksTable.total_page_count)
@@ -64,7 +64,7 @@ async def get_admin_books() -> list[tuple[str, str, int]]:
     
 
 async def get_user_books(user_id: int) -> list[tuple[str, str, int]]:
-    async with Database().session as session:
+    async with database.session as session:
         stmt = (
             select(UserBooksTable.book_id, UserBooksTable.book_title,
                    UserBooksTable.total_page_count)
