77648b91e193f9e1b5709ea7915234f2d974035d
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-11 11:27:53 +0500

    refactor: update create database methods by adding database var for session instead Database class

--- bot/database/methods/create.py
+++ bot/database/methods/create.py
@@ -1,16 +1,16 @@
 from database.models import UsersTable, BookmarksTable, AdminBooksTable, UserBooksTable
-from database.main import Database
+from database.main import database
 
 
 async def add_user(user_id: int) -> None:
-    async with Database().session as session:
+    async with database.session as session:
         session.add(UsersTable(user_id=user_id))
         await session.commit()
 
 
 async def add_user_bookmark(user_id: int, book_id: str,
                             book_title: str, page_number: int) -> None:
-    async with Database().session as session:
+    async with database.session as session:
         session.add(BookmarksTable(user_id=user_id, book_title=book_title,
                                    book_id=book_id, page_number=page_number))
         await session.commit()
@@ -18,7 +18,7 @@ async def add_user_bookmark(user_id: int, book_id: str,
 
 async def add_admin_book(admin_username: str, book_id: str,
                          book_title: str, page_count: int) -> None:
-    async with Database().session as session:
+    async with database.session as session:
         session.add(AdminBooksTable(admin_username=admin_username, book_title=book_title,
                                     book_id=book_id, total_page_count=page_count)) 
         await session.commit()
@@ -26,7 +26,7 @@ async def add_admin_book(admin_username: str, book_id: str,
 
 async def add_user_book(user_id: int, book_id: str, book_title: str,
                         page_count: int, is_admin_book: bool = False) -> None:
-    async with Database().session as session:
+    async with database.session as session:
         session.add(UserBooksTable(user_id=user_id, book_title=book_title,
                                    book_id=book_id, total_page_count=page_count,
                                    current_page_num=1, is_admin_book=is_admin_book))
