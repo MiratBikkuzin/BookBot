9cd322a76f4956610466bb98329cca98decaab4c
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-12 22:05:45 +0500

    feat: add method into book object store class for get book page content

--- bot/services/object_store/methods.py
+++ bot/services/object_store/methods.py
@@ -12,12 +12,20 @@ class BookObjectStore:
     
 
     async def upload_book(self, book_text: str, book_id: str,
-                          is_admin: bool = False) -> dict[int: str]:
+                          is_admin: bool = False) -> dict[int: str] | None:
 
         book: dict[int: str] = prepare_book(book_text)
+        objects: iter[str] = (obj.name for obj in await object_store.list())
 
         for page_num, content in book.items():
             key: str = self.__get_book_page_key(book_id, page_num, is_admin)
+            if key in objects:
+                return
             await object_store.put(name=key, data=content)
 
         return book
+    
+    async def get_book_page_content(self, book_id: str, page_num: int, is_admin: bool = False) -> str:
+        key: str = self.__get_book_page_key(book_id, page_num, is_admin)
+        obr = await object_store.get(key)
+        return obr.data.decode()
