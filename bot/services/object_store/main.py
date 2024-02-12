b506f1d6c4371acf8e19f7d8c040ff67ec860271
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-12 21:43:30 +0500

    feat: add nats object store for bot

---
+++ bot/services/object_store/main.py
@@ -0,0 +1,21 @@
+from config_data.config import nats_settings
+
+from nats.aio.client import Client
+from nats.js import JetStreamContext
+from nats.js.errors import BucketNotFoundError
+from nats.js.object_store import ObjectStore
+
+import nats
+
+
+async def get_object_store() -> None:
+    global object_store
+
+    nc: Client = await nats.connect()
+    js: JetStreamContext = nc.jetstream()
+    
+    try:
+        object_store: ObjectStore = await js.object_store(nats_settings.bucket)
+
+    except BucketNotFoundError:
+        object_store: ObjectStore = await js.create_object_store(nats_settings.bucket)
