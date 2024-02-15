608f5db021b7df61811956a6545cd88cd8156b6d
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-15 22:50:40 +0500

    refactor: update class BookmarksKeyboard by changing create_bookmarks_kb method

--- bot/keyboards/bookmarks_kb.py
+++ bot/keyboards/bookmarks_kb.py
@@ -1,25 +1,25 @@
-from database.methods.get import get_user_bookmarks
-from services.s3_file_handling import get_book_s3
-from keyboards.keyboard_utils import BookmarksCallbackFactory, DelBookmarksCallbackFacotry
+from database.methods.get import get_user_books_with_bookmarks
+from services.object_store import BookObjectStore
+from keyboards.kb_utils import (BookMarkCallbackFactory, EditBookMarkCallbackFactory,
+                                BookPageMarkCallbackFactory, EditBookPageMarkCallbackFactory)
 from lexicon.lexicon import LEXICON_RU
 
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-class BookmarkFactory:
+class BookmarksKeyboard:
 
     @staticmethod
-    async def create_bookmarks_kb(user_id: int, book_title: str) -> InlineKeyboardMarkup:
+    async def create_bookmarks_kb(user_id: int) -> InlineKeyboardMarkup:
 
         kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
-        book: dict[int: str] = await get_book_s3(book_title, user_id)
+        books: list[tuple[str, str]] = await get_user_books_with_bookmarks(user_id)
 
-        for button in sorted(await get_user_bookmarks(user_id, book_title)):
+        for book_id, book_title in books:
             kb_builder.row(InlineKeyboardButton(
-                text=f'{button} - {book[button][:85]}',
-                callback_data=BookmarksCallbackFactory(page_number=button,
-                                                       book_title=book_title).pack()
+                text=book_title,
+                callback_data=BookMarkCallbackFactory(book_id)
             ))
 
         kb_builder.row(
@@ -35,32 +35,32 @@ class BookmarkFactory:
 
         return kb_builder.as_markup()
     
-    @staticmethod
-    async def create_edit_kb(user_id: int, book_title: str) -> InlineKeyboardMarkup:
+    # @staticmethod
+    # async def create_edit_kb(user_id: int, book_title: str) -> InlineKeyboardMarkup:
 
-        kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
-        book: dict[int: str] = await get_book_s3(book_title, user_id)
+    #     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    #     book: dict[int: str] = await s3_manager.get_book_s3(book_title, user_id)
 
-        for button in sorted(await get_user_bookmarks(user_id, book_title)):
-            kb_builder.row(InlineKeyboardButton(
-                text=f"{LEXICON_RU['del']} {button} - {book[button][:85]}",
-                callback_data=DelBookmarksCallbackFacotry(page_number=button,
-                                                          book_title=book_title).pack()
-            ))
+    #     for button in sorted(await get_user_bookmarks(user_id, book_title)):
+    #         kb_builder.row(InlineKeyboardButton(
+    #             text=f"{LEXICON_RU['del']} {button} - {book[button][:85]}",
+    #             callback_data=DelBookmarksCallbackFactory(page_number=button,
+    #                                                       book_title=book_title).pack()
+    #         ))
 
-        kb_builder.row(InlineKeyboardButton(
-            text=LEXICON_RU['cancel'],
-            callback_data='cancel'
-            ))
+    #     kb_builder.row(InlineKeyboardButton(
+    #         text=LEXICON_RU['cancel'],
+    #         callback_data='cancel'
+    #         ))
 
-        return kb_builder.as_markup()
+    #     return kb_builder.as_markup()
     
-    @staticmethod
-    def back_from_bookmark_kb() -> InlineKeyboardMarkup:
+    # @staticmethod
+    # def back_from_bookmark_kb() -> InlineKeyboardMarkup:
         
-        back_button: InlineKeyboardButton = InlineKeyboardButton(
-            text=LEXICON_RU['back_bookmark_button'],
-            callback_data='back_bookmark'
-        )
+    #     back_button: InlineKeyboardButton = InlineKeyboardButton(
+    #         text=LEXICON_RU['back_bookmark_button'],
+    #         callback_data='back_bookmark'
+    #     )
 
-        return InlineKeyboardMarkup(inline_keyboard=[[back_button]])
+    #     return InlineKeyboardMarkup(inline_keyboard=[[back_button]])
