dc184e817a463d5f3e1866c63d1e1d1e1cc6be17
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-16 22:57:36 +0500

    refactor: delete filter IsAddBookmarkCallbackData

--- bot/filters/filters.py
+++ bot/filters/filters.py
@@ -18,13 +18,6 @@ class IsCorrectAdminBook(BaseFilter):
                         'file_format': file_format}
 
 
-class IsAddBookmarkCallbackData(BaseFilter):
-    async def __call__(self, callback: CallbackQuery) -> dict[str: int] | None:
-        callback_data: str = callback.data
-        if '/' in callback_data and callback_data.replace('/', '', 1).isdigit():
-            return {'bookmark_page': int(callback_data.split('/')[0])}
-
-
 class IsDigitCallbackData(BaseFilter):
     async def __call__(self, callback: CallbackQuery) -> bool:
         return callback.data.isdigit()
