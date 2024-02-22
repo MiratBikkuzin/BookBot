5e3c0b93a5e28ecf34720111cf8c3cb06674261a
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-22 22:32:46 +0500

    feat: add handler for /add_book command

--- bot/handlers/user/book_handlers/add_book_handlers.py
+++ bot/handlers/user/book_handlers/add_book_handlers.py
@@ -1,5 +1,6 @@
 from database.methods.create import add_user_book
 from states.states import FSMUserBook, default_state
+from filters.filters import IsCorrectBook
 from services.object_store import BookObjectStore
 from services.file_handling import parse_fb2, get_book_text
 from lexicon.lexicon import LEXICON_RU
@@ -12,3 +13,11 @@ from aiogram.fsm.context import FSMContext
 
 
 router: Router = Router(name=__name__)
+
+
+@router.message(Command(commands='add_book'), StateFilter(default_state))
+async def process_add_book_command(message: Message, state: FSMContext):
+    await message.answer(text=LEXICON_RU[message.text])
+    await state.set_state(FSMUserBook.user_book_send)
+
+
