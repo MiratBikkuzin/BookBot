04e1b89eb458570a2032e32216ba999f0ee243c6
Mirat Bikkuzin <mirat1909@gmail.com>
2024-02-29 22:17:02 +0500

    refactor: update handler for /start command by adding num_books_to_add param in funcs

--- bot/handlers/user/start_handlers.py
+++ bot/handlers/user/start_handlers.py
@@ -1,5 +1,5 @@
 from database.methods.create import add_user
-from database.methods.get import get_user
+from database.methods.get import get_user_info
 from lexicon.lexicon import LEXICON_RU
 
 from aiogram import Router
@@ -16,8 +16,8 @@ async def process_start_command(message: Message) -> None:
     user_id: int = message.from_user.id
     firstname: str = message.from_user.first_name
 
-    if not await get_user(user_id):
-        await add_user(user_id)
+    if not await get_user_info(user_id):
+        await add_user(user_id, num_books_to_add=5)
         await message.answer(LEXICON_RU[message.text] % firstname)
 
     else:
