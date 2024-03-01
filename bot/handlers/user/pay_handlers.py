a36ff8166e7506ac7347e133459a3467aec9264b
Mirat Bikkuzin <mirat1909@gmail.com>
2024-03-01 15:54:53 +0500

    feat: add new handler for process select unlimited books to add and delete class PaymentKeyboards and method create_replenish_kb

--- bot/handlers/user/pay_handlers.py
+++ bot/handlers/user/pay_handlers.py
@@ -4,7 +4,7 @@ from keyboards.pay_kb import (
     max_tip_amount,
     tip_amounts,
     start_parameter,
-    PaymentKeyboards
+    create_payment_kb
 )
 from database.methods.update import update_quantity_to_add_books
 from config_data.config import bot_settings
@@ -22,7 +22,7 @@ router: Router = Router()
 async def process_send_selection_quantity_for_top_up(message: Message):
     await message.answer(
         text=LEXICON_RU['top_up_quantity_text'],
-        reply_markup=PaymentKeyboards.create_payment_kb()
+        reply_markup=create_payment_kb()
     )
 
 
@@ -31,12 +31,28 @@ async def process_select_ten_books_to_add(callback: CallbackQuery):
     await callback.message.answer_invoice(
         title=LEXICON_RU['ten_books_invoice_title'],
         description=LEXICON_RU['invoice_description'],
-        payload='1',
+        payload='10',
         provider_token=bot_settings.payment_token,
         currency='RUB',
         prices=[ten_books_price],
         max_tip_amount=max_tip_amount,
         suggested_tip_amounts=tip_amounts,
-        start_parameter=start_parameter,
-        reply_markup=PaymentKeyboards.create_replenish_kb()
+        start_parameter=start_parameter
     )
+    await callback.answer()
+
+
+@router.callback_query(F.data == 'unlimited_books_add')
+async def process_select_unlimited_books_to_add(callback: CallbackQuery):
+    await callback.message.answer_invoice(
+        title=LEXICON_RU['unlimited_books_invoice_title'],
+        description=LEXICON_RU['invoice_description'],
+        payload='infinity',
+        provider_token=bot_settings.payment_token,
+        currency='RUB',
+        prices=[unlimited_books_price],
+        max_tip_amount=max_tip_amount,
+        suggested_tip_amounts=tip_amounts,
+        start_parameter=start_parameter
+    )
+    await callback.answer()
--- bot/keyboards/pay_kb.py
+++ bot/keyboards/pay_kb.py
@@ -11,23 +11,15 @@ tip_amounts: list[int] = [10 * 100, 20 * 100, 40 * 100, 80 * 100]
 start_parameter: str = 'chitalkabot'
 
 
-class PaymentKeyboards:
+def create_payment_kb() -> InlineKeyboardMarkup:
 
-    @staticmethod
-    def create_payment_kb() -> InlineKeyboardMarkup:
-
-        ten_books_add_button = InlineKeyboardButton(text='10', callback_data='ten_books_add')
-        unlimited_books_add_button = InlineKeyboardButton(text='Бесконечное количество',
-                                                          callback_data='unlimited_books_add')
+    ten_books_add_button = InlineKeyboardButton(text='10', callback_data='ten_books_add')
+    unlimited_books_add_button = InlineKeyboardButton(
+        text='Бесконечное количество',
+        callback_data='unlimited_books_add'
+    )
         
-        inline_keyboard: list = [[ten_books_add_button],
-                                 [unlimited_books_add_button]]
+    inline_keyboard: list = [[ten_books_add_button],
+                             [unlimited_books_add_button]]
 
-        return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
-
-    @staticmethod
-    def create_replenish_kb() -> InlineKeyboardMarkup:
-        pay_button = InlineKeyboardButton(text='Заплатить', pay=True)
-        back_button = InlineKeyboardButton(text=LEXICON_RU['back_button'],
-                                           callback_data='back_from_replenishment')
-        return InlineKeyboardMarkup(inline_keyboard=[[pay_button], [back_button]])
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
