8238903121e44058a2562d660123d31a04077e17
Mirat Bikkuzin <mirat1909@gmail.com>
2024-03-03 00:40:11 +0500

    feat: add new utils func for get profile command text

--- bot/utils/book_utils.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-
-
-def get_book_id(book_title: str) -> str:
-    hash_obj = hashlib.sha1(bytes(book_title, 'utf-8'))
-    return hash_obj.hexdigest()
---
+++ bot/utils/utils.py
@@ -0,0 +1,21 @@
+import hashlib
+from lexicon.lexicon import LEXICON_RU, PROFILE_LEXICON_RU
+
+
+def get_book_id(book_title: str) -> str:
+    hash_obj = hashlib.sha1(bytes(book_title, 'utf-8'))
+    return hash_obj.hexdigest()
+
+
+def get_profile_command_text(name: str, user_id: int, books_num: int,
+                             num_books_to_add: int | str, bookmarks_num: int) -> str:
+    
+    if isinstance(num_books_to_add, str):
+        num_books_to_add: str = LEXICON_RU['unlimited']
+
+    return f"{PROFILE_LEXICON_RU['profile']}\n\n" \
+           f"{PROFILE_LEXICON_RU['name']} {name}\n" \
+           f"{PROFILE_LEXICON_RU['id']} {user_id}\n\n" \
+           f"{PROFILE_LEXICON_RU['books_started']} {books_num}\n" \
+           f"{PROFILE_LEXICON_RU['remaining_additions']} {num_books_to_add}\n" \
+           f"{PROFILE_LEXICON_RU['bookmarks_number']} {bookmarks_num}"
