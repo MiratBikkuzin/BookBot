2b253a4309f45fa470982fec214670e177da5beb
Mirat Bikkuzin <mirat1909@gmail.com>
2024-03-14 22:03:00 +0500

    feat: change handler for command /add_book by setting state for send book author instead book info

--- bot/handlers/user/book_handlers/add_book_handlers.py
+++ bot/handlers/user/book_handlers/add_book_handlers.py
@@ -1,3 +1,6 @@
+'''ПЕРЕДЕЛАТЬ СИСТЕМУ ДОБАВЛЕНИЯ КНИГ,
+ДОБАВИВ БОЛЬШЕ СОСТОЯНИЙ
+И СПРАШИВАЯ ТАКЖЕ ИНИЦИАЛЫ АВТОРА КНИГИ'''
 from database.methods.create import add_user_book
 from database.methods.get import get_user_info, get_user_book_info
 from database.methods.update import update_quantity_to_add_books
@@ -28,7 +31,7 @@ async def process_add_book_command(message: Message, state: FSMContext):
     if isinstance(current_num_books_to_add, str) or current_num_books_to_add > 0:
         await message.answer(text=LEXICON_RU[message.text],
                              reply_markup=BooksKeyboard.create_cancel_add_book_kb())
-        await state.set_state(FSMUserBook.user_book_send)
+        await state.set_state(FSMUserBook.send_book_author)
 
     else:
         await message.answer(text=LEXICON_RU['add_book_error'], reply_markup=create_payment_kb())
@@ -43,40 +46,40 @@ async def cancel_add_book_process(callback: CallbackQuery, state: FSMContext):
     )
 
 
-@router.message(StateFilter(FSMUserBook.user_book_send), IsCorrectBook())
-async def process_user_send_book(message: Message, bot: Bot, book_file_id: str,
-                                 book_title: str, file_format: str, state: FSMContext):
+# @router.message(StateFilter(FSMUserBook.user_book_send), IsCorrectBook())
+# async def process_user_send_book(message: Message, bot: Bot, book_file_id: str,
+#                                  book_title: str, file_format: str, state: FSMContext):
     
-    user_id: int = message.from_user.id
-    book_id: str = get_book_id(book_title.lower())
+#     user_id: int = message.from_user.id
+#     book_id: str = get_book_id(book_title.lower())
 
-    if await get_user_book_info(user_id, book_id):
-        await message.answer(text=LEXICON_RU['users_book_in_stock_warning'])
+#     if await get_user_book_info(user_id, book_id):
+#         await message.answer(text=LEXICON_RU['users_book_in_stock_warning'])
 
-    else:
+#     else:
 
-        await message.answer(text=LEXICON_RU['wait_user_book_download'])
+#         await message.answer(text=LEXICON_RU['wait_user_book_download'])
 
-        book_text: str = get_book_text(await bot.download(book_file_id))
+#         book_text: str = get_book_text(await bot.download(book_file_id))
 
-        if file_format == 'fb2':
-            book_text: str = parse_fb2(book_text)
+#         if file_format == 'fb2':
+#             book_text: str = parse_fb2(book_text)
 
-        book: dict[int: str] = await BookObjectStore.upload_book(book_text, book_id)
-        await add_user_book(user_id, book_id, book_title, page_count=len(book))
+#         book: dict[int: str] = await BookObjectStore.upload_book(book_text, book_id)
+#         await add_user_book(user_id, book_id, book_title, page_count=len(book))
 
-        num_books_to_add: int | str = await get_user_info(user_id)
+#         num_books_to_add: int | str = await get_user_info(user_id)
 
-        if isinstance(num_books_to_add, int):
-            await update_quantity_to_add_books(user_id, num_books_to_add - 1)
+#         if isinstance(num_books_to_add, int):
+#             await update_quantity_to_add_books(user_id, num_books_to_add - 1)
 
-        await message.answer(text=LEXICON_RU['user_book_download_end'])
-        await state.clear()
+#         await message.answer(text=LEXICON_RU['user_book_download_end'])
+#         await state.clear()
 
 
-@router.message(StateFilter(FSMUserBook.user_book_send), ~IsCorrectBook())
-async def not_user_send_book_warning(message: Message):
-    await message.answer(
-        text=LEXICON_RU['other_format_send_book'],
-        reply_markup=BooksKeyboard.create_cancel_add_book_kb()
-    )
+# @router.message(StateFilter(FSMUserBook.user_book_send), ~IsCorrectBook())
+# async def not_user_send_book_warning(message: Message):
+#     await message.answer(
+#         text=LEXICON_RU['other_format_send_book'],
+#         reply_markup=BooksKeyboard.create_cancel_add_book_kb()
+#     )
