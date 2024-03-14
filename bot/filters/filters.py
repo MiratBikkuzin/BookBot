dc2bdb0e8e8afe451c955d2d9c461714185aa694
Mirat Bikkuzin <mirat1909@gmail.com>
2024-03-14 22:25:41 +0500

    refactor: change filter IsCorrectBook and rename it to IsCorrectBookFormat

--- bot/filters/filters.py
+++ bot/filters/filters.py
@@ -9,10 +9,8 @@ class IsAdmin(BaseFilter):
         return message.from_user.id in bot_settings.admin_ids_lst
     
 
-class IsCorrectBook(BaseFilter):
+class IsCorrectBookFormat(BaseFilter):
     async def __call__(self, message: Message) -> bool | None:
-        if message.document and message.caption:
-            file_format: str = message.document.file_name.split('.')[-1]
-            if file_format in ('fb2', 'txt'):
-                return {'book_file_id': message.document.file_id, 'book_title': message.caption,
-                        'file_format': file_format}
+        file_format: str = message.document.file_name.split('.')[-1]
+        if file_format in ('fb2', 'txt'):
+            return {'book_file_id': message.document.file_id, 'file_format': file_format}
