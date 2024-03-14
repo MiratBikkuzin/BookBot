40c0a35f6cf47d6d47c3ee2ee55b17ff39351397
Mirat Bikkuzin <mirat1909@gmail.com>
2024-03-14 22:55:22 +0500

    feat: add new states in StatesGroup FSMAdminBook for admin book info

--- bot/states/states.py
+++ bot/states/states.py
@@ -2,7 +2,9 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class FSMAdminBook(StatesGroup):
-    admin_book_send = State()
+    send_book_author = State()
+    send_book_name = State()
+    send_book_file = State()
 
 
 class FSMUserBook(StatesGroup):
