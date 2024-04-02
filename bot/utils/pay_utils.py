42fcba2122dc1af1e5020f7c1bdec23e1bc30918
Mirat Bikkuzin <mirat1909@gmail.com>
2024-04-02 19:07:12 +0500

    feat: add func for generate payment robokassa link

--- bot/utils/pay_utils.py
+++ bot/utils/pay_utils.py
@@ -1,7 +1,6 @@
-import decimal
-import hashlib
+from config_data.config import merchant_settings
 from urllib import parse
-from urllib.parse import urlparse
+import hashlib
 
 
 def _calculate_signature(*args) -> str:
@@ -9,3 +8,39 @@ def _calculate_signature(*args) -> str:
     return hashlib.md5(':'.join(str(arg) for arg in args).encode()).hexdigest()
 
 
+def generate_payment_link(
+        user_id: int,
+        num_books_to_add: int | str,
+        price: int, # Cost of goods, RU
+        description: str, # Description of the purchase
+        inv_id: int = 0,
+        price_curr: str = 'RUB',
+        is_test: int = 0,
+        robokassa_payment_url: str = 'https://auth.robokassa.ru/Merchant/Index.aspx'
+) -> str:
+    
+    login: str = merchant_settings.login
+    password_1: str = merchant_settings.password_1
+    shp_numbooks: str = f'Shp_numbooks={num_books_to_add}'
+    shp_userid: str = f'Shp_userid={user_id}'
+    shp_params: list[str, str] = sorted((shp_numbooks, shp_userid))
+
+    data = {
+        'MerchantLogin': login,
+        'OutSum': price,
+        'InvId': inv_id,
+        'Description': description,
+        'Shp_numbooks': num_books_to_add,
+        'Shp_userid': user_id,
+        'IsTest': is_test
+    }
+    
+    if price_curr != 'RUB':
+        signature = _calculate_signature(login, price, inv_id, price_curr, password_1, *shp_params)
+        data['OutSumCurrency'] = price_curr
+
+    else:
+        signature = _calculate_signature(login, price, inv_id, password_1, *shp_params)
+
+    data['SignatureValue'] = signature
+    return f'{robokassa_payment_url}?{parse.urlencode(data)}'
