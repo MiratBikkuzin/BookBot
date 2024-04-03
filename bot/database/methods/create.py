941c5983a17da16eab942bce73adc2f14a92aa66
Mirat Bikkuzin <mirat1909@gmail.com>
2024-04-03 21:28:21 +0500

    feat: add create database method for add user payment info in postgres model PaymentsInfo

--- bot/database/methods/create.py
+++ bot/database/methods/create.py
@@ -1,4 +1,10 @@
-from database.models import UsersTable, BookmarksTable, AdminBooksTable, UserBooksTable
+from database.models import (
+    UsersTable,
+    BookmarksTable,
+    AdminBooksTable,
+    UserBooksTable,
+    PaymentsInfoTable
+)
 from database.main import database
 
 
@@ -31,4 +37,10 @@ async def add_user_book(user_id: int, book_id: str, book_author: str, book_title
         session.add(UserBooksTable(user_id=user_id, book_author=book_author, book_title=book_title,
                                    book_id=book_id, total_page_count=page_count,
                                    current_page_num=1, is_admin_book=is_admin_book))
+        await session.commit()
+
+
+async def add_user_payment_info(user_id: int, inv_id: int) -> None:
+    async with database.session as session:
+        session.add(PaymentsInfoTable(user_id=user_id, invoice_id=inv_id))
         await session.commit()
