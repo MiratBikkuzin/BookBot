0822d9bdcdcef45e509ea141a2839e8f46493060
Mirat Bikkuzin <mirat1909@gmail.com>
2024-04-04 21:12:17 +0500

    feat: add new func for create payment keyboard

--- bot/keyboards/pay_kb.py
+++ bot/keyboards/pay_kb.py
@@ -2,7 +2,7 @@ from lexicon.lexicon import LEXICON_RU
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-def create_payment_kb() -> InlineKeyboardMarkup:
+def create_choice_books_payment_kb() -> InlineKeyboardMarkup:
 
     ten_books_add_button = InlineKeyboardButton(text='10', callback_data='ten_books_add')
     unlimited_books_add_button = InlineKeyboardButton(
@@ -14,3 +14,17 @@ def create_payment_kb() -> InlineKeyboardMarkup:
                              [unlimited_books_add_button]]
 
     return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+
+
+def create_payment_kb(payment_link: str, price: int) -> InlineKeyboardMarkup:
+    
+    link_button = InlineKeyboardButton(
+        text=LEXICON_RU['books_price_button'] % price,
+        url=payment_link
+    )
+    check_payment_button = InlineKeyboardButton(
+        text=LEXICON_RU['check_payment_button'],
+        callback_data='check-payment-process'
+    )
+
+    return InlineKeyboardMarkup(inline_keyboard=[[link_button], [check_payment_button]])
